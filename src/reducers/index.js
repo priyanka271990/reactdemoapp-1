9b6e8032e80f2dac3c99f9758977e701245427aa
Gaurav Joshi <gauravjoshi786786@gmail.com>
2018-03-29 23:57:32 +0530

    Television mock-up created

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import {combineReducers} from 'redux';
 import UserReducers from './user.reducers';
 import MovieReducers from './movie.reducers';
 import ActiveUserReducers from './active.user.reducers';
+import TelevisionReducers from './television.reducers';
 
 const allReducers = combineReducers({
-	user: UserReducers,
-	movie: MovieReducers,
 	activeUser: ActiveUserReducers,
+	movie: MovieReducers,
+	television: TelevisionReducers,
+	user: UserReducers,
 });
 
 export default allReducers;
